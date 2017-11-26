61a6504d3cf49ee267083bc03e0fae189f58adf8
dobarDanDan <dobar.dan.postovanje@gmail.com>
2017-11-26 09:28:49 +0100

    add Header.jsx and TableWrapper.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header/Header.jsx';
 
 export default class App extends React.Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
+     <div>
+        <Header />
+      </div>
+    );
   }
 }
