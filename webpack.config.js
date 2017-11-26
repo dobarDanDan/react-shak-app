b39a545fb99dadb2b73cf9f96fa7b3331d6c1bd5
dobarDanDan <dobar.dan.postovanje@gmail.com>
2017-11-26 10:26:52 +0100

    webpack.config.js style repair

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,19 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { 
+        test: /\.js$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/ 
+      },
+
+      { 
+        test: /\.jsx$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/ 
+      }
     ]
   },
+
   plugins: [HtmlWebpackPluginConfig]
 };
